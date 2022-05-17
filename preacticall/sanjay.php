bd5a6a2bca0916e04f4b8eca8a79619b69572ee1
sk7dihora <83339537+sk7dihora@users.noreply.github.com>
2022-05-17 10:46:34 +0530

    Add files via upload

---
+++ preacticall/sanjay.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+
+<body>
+    <h2> from</h2>
+    <hr>
+    
+            <form action="s.html" method="post">
+            name: <input type="text" name="name">
+            email: <input type="text" name="email">
+            <input type="submit">
+
+            </form>
+
+
+    
+</body>
+</html>
---
+++ preacticall/sanju.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+class Car {
+  public $color;
+  public $model;
+  public function __construct($color, $model) {
+    $this->color = $color;
+    $this->model = $model;
+  }
+  public function message() {
+    return "My car is a " . $this->color . " " . $this->model . "!";
+  }
+}
+
+$myCar = new Car("black", "Volvo");
+echo $myCar -> message();
+echo "<br>";
+$myCar = new Car("red", "Toyota");
+echo $myCar -> message();
+echo "<br>";
+echo strlen("sanjay");
+echo " <br>";
+//echo str_word_count("sanjay rathod");
+
+//echo strpos("sanjay rathod d", "rathod");
+echo str_replace("sanjay","sanju","rathod sanjay");
+?>
+
+</body>
+</html>
